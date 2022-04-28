dbf6e5b7ac1b4c13d3c1e99f3e3c40eaf4b2275c
guven.uysal <guven.uysal@teknosol.com.tr>
2022-04-28 11:23:19 +0300

    Initial commit

---
+++ src/Projections/Sozluk.Projections.FavoriteService/Program.cs
@@ -0,0 +1,7 @@
+using Sozluk.Projections.FavoriteService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => { services.AddHostedService<Worker>(); })
+    .Build();
+
+await host.RunAsync();
---
+++ src/Projections/Sozluk.Projections.UserService/Program.cs
@@ -0,0 +1,7 @@
+using Sozluk.Projections.UserService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => { services.AddHostedService<Worker>(); })
+    .Build();
+
+await host.RunAsync();
